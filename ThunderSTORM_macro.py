556600aaba4aa6030e51d4df3d7fd20f5e60e418
Yousefrofa <yousefrofa@gmail.com>
2025-06-10 11:39:56 +0400

    Updating shot noise and ThunderSTORM macro files

---
+++ ThunderSTORM_macro.py
@@ -0,0 +1,33 @@
+import fijibin
+import json
+import os
+import pandas as pd
+
+folder = "Z:\Yousef(yma9167)\DNA_ImgDet\dsDNA50 1 fM"
+sigma = 1.6  # Default value recommended by library doc is 1.6
+
+tif_files = []
+for file in os.listdir():
+    if file.endswith(".tif"):
+        tif_files.append(file)
+
+# for file in tif_files:
+#     macro = f'''
+#     open("{folder}");
+# selectImage("{file});
+# run("Run analysis", "filter=[Lowered Gaussian filter] sigma={sigma} detector=[Local maximum] connectivity=8-neighbourhood threshold=std(Wave.F1) estimator=[PSF: Integrated Gaussian] sigma=1.6 fitradius=3 method=[Least squares] full_image_fitting=false mfaenabled=false renderer=[Averaged shifted histograms] magnification=5.0 colorizez=false threed=false shifts=2 repaint=50");
+# selectImage("Averaged shifted histograms");
+# run("Export results", "sigma=true intensity=true offset=true saveprotocol=true filepath=[{folder}\output.csv] x=true y=true bkgstd=true id=false uncertainty=true fileformat=[CSV (comma separated)] frame=true");
+#     '''
+#     print(file)
+#     fijibin.macro.run(macro)
+
+
+m = f'''
+open("C:\\Users\\youse\\Desktop\\output.tif");
+selectImage("output.tif");
+run("Run analysis", "filter=[Gaussian filter] sigma=1.6 detector=[Local maximum] connectivity=8-neighbourhood threshold=std(Wave.F1) estimator=[PSF: Integrated Gaussian] sigma=1.6 fitradius=3 method=[Least squares] full_image_fitting=false mfaenabled=false renderer=[Averaged shifted histograms] magnification=5.0 colorizez=false threed=false shifts=2 repaint=50");
+run("Export results", "sigma=true intensity=true offset=true saveprotocol=true filepath=C:\\Users\\youse\\Desktop\\output.csv x=true y=true bkgstd=true id=false uncertainty=true fileformat=[CSV (comma separated)] frame=true");
+close();
+'''
+fijibin.macro.run(m)
---
+++ shot_noise.py
@@ -0,0 +1,15 @@
+import scipy
+import cv2
+from numpy.fft import fft2, fftshift, ifftshift
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import animation
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('output.tif')
+# print(np.unravel_index(np.argmax(image, axis=None), image.shape))
+
+imageplusshotnoise = np.random.poisson(lam=image, size=None)
+plt.figure(figsize=(10, 5))
+plt.imshow(imageplusshotnoise)
+plt.show()
